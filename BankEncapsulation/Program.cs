3daec26489077c34123450a16134d8ea8a31ff2c
Shayn Coffey <Shayncoffey30@gmail.com>
2024-09-19 23:10:44 -0400

    completed exercise

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             var michealsAccount = new BankAccount();
+            
+            Console.WriteLine("How much would you like to deposit?");
+           
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            michealsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now {michealsAccount.GetBalance()}");
         }
     }
 }
